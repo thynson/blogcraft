7083531364962f7bf4dac0bd680e8a7cbf0d8ed5
Thynson <lanxingcan@gmail.com>
2013-03-25 16:39:46 +0800

    wip scripts
    Signed-off-by: Thynson <lanxingcan@gmail.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,32 @@
 var x = require('bootstrap');
 var $ = require('jquery');
 
+var isModify = false;
+
+$('#btn-new-post').click(function(e) {
+	isModify = false;
+	$('#form-new-post').reset();
+});
+
+$('#form-new-post').submit(function(e) {
+	e.preventDefault();
+	var postdata = { title : $('#form-new-post input[name="title"]').val()
+				   , body : $('#form-new-post textarea[name="body"]').val() };
+
+	//TODO: Veryfy data
+
+	if (!isModify) {
+		postdata.id = $('#form-new-post input[name="id"]').val();
+	}
+
+
+	$.post('/post', postdata, function(data) {
+		$('a[href="#manage"]').tab('show');
+	}).fail(function() {
+		alert('Failed');
+	});
+});
+
 $('a[data-toggle="tab"]').on('shown', function(e) {
 
 	var x = $(e.target);
@@ -9,19 +35,51 @@ $('a[data-toggle="tab"]').on('shown', function(e) {
 	var postListTable = $('#post-list tbody');
 
 	switch(currentTabId) {
+
+	case '#edit':
+		(function() {
+		});
+
 	case '#manage':
 		(function(){
 			$.get('/ajax/post', function(data) {
 				postListTable.empty();
 				$.each(data, function(idx, value) {
 					setTimeout(function(){
-						postListTable.append(
-							$('<tr/>').append($('<td/>').text(value.title))
-							.append($('<td/>').text(value.createdDate))
-							.append($('<td/>'))
-							.attr('id', value._id)
-						);
-					}, 0);
+						var tr = $('<tr/>').appendTo(postListTable);
+						$('<td/>').text(value.title).appendTo(tr);
+						$('<td/>').text(value.modifiedDate).appendTo(tr);
+
+						var td = $('<td/>').appendTo(tr);
+						var buttonGroup = $('<div/>')
+										  .addClass('btn-group')
+										  .appendTo(td);
+
+						$('<button/>')
+						.addClass('btn')
+						.text('Remove')
+						.click(function(e) {
+							$.post('/ajax/post/delete'
+							, { id : value._id}
+							, function(){
+								tr.remove();
+							}).fail(function(){
+								alert('Faild');
+							});
+						}).appendTo(buttonGroup);
+
+						$('<button/>')
+						.addClass('btn')
+						.text('Edit')
+						.click(function(e) {
+							isModify = true;
+							$('#edit input[name="id"]').val(value._id);
+							$('#edit input[name="title"]').val(value.title);
+							$('#edit textarea[name="body"]').val(value.source);
+							$('a[href="#edit"]').tab('show');
+						}).appendTo(buttonGroup);
+
+					}, idx);
 				});
 			}).fail(function() {
 				// TODO:
@@ -38,3 +96,5 @@ $('a[data-toggle="tab"]').on('shown', function(e) {
 	};
 
 });
+
+
