620071bc0b19bfae740276007c8bca6436575675
Thynson <lanxingcan@gmail.com>
2013-03-25 15:32:56 +0800

    Initial implement of manage page
    Signed-off-by: Thynson <lanxingcan@gmail.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,40 @@
 var x = require('bootstrap');
 var $ = require('jquery');
+
+$('a[data-toggle="tab"]').on('shown', function(e) {
+
+	var x = $(e.target);
+	var currentTabId = $(e.target).attr('href');
+	var relatedTabId = $(e.relatedTarget).attr('href');
+	var postListTable = $('#post-list tbody');
+
+	switch(currentTabId) {
+	case '#manage':
+		(function(){
+			$.get('/ajax/post', function(data) {
+				postListTable.empty();
+				$.each(data, function(idx, value) {
+					setTimeout(function(){
+						postListTable.append(
+							$('<tr/>').append($('<td/>').text(value.title))
+							.append($('<td/>').text(value.createdDate))
+							.append($('<td/>'))
+							.attr('id', value._id)
+						);
+					}, 0);
+				});
+			}).fail(function() {
+				// TODO:
+			});
+		})();
+		break;
+	};
+
+
+	switch(relatedTabId) {
+	case '#manage':
+		postListTable.empty();
+
+	};
+
+});
